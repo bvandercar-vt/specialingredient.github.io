40e38e422a0a293516efad3a6455de3848d4cedd
bvandercar-vt <blake.vandercar@validatek.com>
2023-08-17 11:45:49 -0600

    use overflow-y: auto, set all playlist blocks to maybe scroll

--- scripts/script.js
+++ scripts/script.js
@@ -15,6 +15,10 @@ function isScrolledToBottom(element, offset = 0) {
     return element.scrollTop > (element.scrollHeight - element.offsetHeight - offset)
 }
 
+function isScrollableY(element) {
+    return element.scrollHeight > element.clientHeight
+}
+
 function getWindowWidth() {
     return window.innerWidth || document.documentElement.clientWidth ||
         document.body.clientWidth;
@@ -24,21 +28,77 @@ function isMobile() {
     return getWindowWidth() < MOBILE_WIDTH
 }
 
-function init() {
-    // All anchors
-    const allAnchors = document.getElementsByTagName("a")
-    Array.from(allAnchors).forEach((el) => {
-        // Open in new tab
-        el.target = "_blank"
-        el.rel = "noreferrer noopener"
-    })
+function waitForElements(
+    /** @type {() => NodeListOf<any>} */
+    checkFunction,
+    /** @type {number} */
+    expectedLength) {
+    return new Promise(resolve => {
+        const items = checkFunction()
+        if (items.length === expectedLength) {
+            return resolve(items);
+        }
 
-    // Soundcloud stuff
-    SC.initialize({
-        client_id: 'DgFeY88vapbGCcK7RrT2E33nmNQVWX82'
+        const observer = new MutationObserver(mutations => {
+            const items = checkFunction()
+            if (items.length === expectedLength) {
+                resolve(items);
+                observer.disconnect();
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
     });
+}
 
-    Array.from(document.getElementsByClassName("transform-to-sc-item")).forEach((el) => {
+function setPlaylistTitlesCollapsable() {
+    function setCollapsed(
+        /** @type {boolean} */
+        collapsed,
+        /** @type {HTMLDivElement} */
+        playlistTitleEl) {
+        const collapseContent = playlistTitleEl.parentElement.getElementsByClassName('playlist-items')[0]
+        const collapseArrow = playlistTitleEl.parentElement.getElementsByClassName('collapse-caret')[0]
+        if (collapsed) {
+            collapseContent.classList.add('hidden')
+            collapseArrow.classList.remove("open")
+        } else {
+            collapseContent.classList.remove('hidden')
+            collapseArrow.classList.add("open")
+        }
+    }
+
+    // Collapsable titles
+    const playlistTitles = document.getElementsByClassName("playlist-title")
+    Array.from(playlistTitles).forEach((playlistTitle) => {
+        const collapseArrow = document.createElement("span")
+        collapseArrow.classList = "fa fa-lg fa-caret-down collapse-caret"
+        playlistTitle.appendChild(collapseArrow)
+        setCollapsed(isMobile(), playlistTitle)
+
+        playlistTitle.addEventListener("click", function () {
+            const collapseContent = this.parentElement.getElementsByClassName('playlist-items')[0]
+            const isCollapsed = collapseContent.classList.contains('hidden')
+            setCollapsed(!isCollapsed, this)
+
+            if (isMobile() && isCollapsed) {
+                Array.from(playlistTitles).forEach((otherPlaylistTitleEl) => {
+                    if (otherPlaylistTitleEl !== this) {
+                        setCollapsed(true, otherPlaylistTitleEl)
+                    }
+                })
+            }
+
+        });
+    })
+}
+
+async function setScTracksElements(SC) {
+    const transformItems = document.getElementsByClassName("transform-to-sc-item")
+    Array.from(transformItems).forEach((el) => {
         SC.oEmbed(el.getAttribute("data-sc-link"), { auto_play: false, maxheight: 150 }).then((
             /**
              * @type { { 
@@ -100,54 +160,14 @@ function init() {
         });
     })
 
-    function setCollapsed(
-        /** @type {boolean} */
-        collapsed,
-        /** @type {HTMLDivElement} */
-        playlistTitleEl) {
-        const collapseContent = playlistTitleEl.parentElement.getElementsByClassName('playlist-items')[0]
-        const collapseArrow = playlistTitleEl.parentElement.getElementsByClassName('collapse-caret')[0]
-        if (collapsed) {
-            collapseContent.classList.add('hidden')
-            collapseArrow.classList.remove("open")
-        } else {
-            collapseContent.classList.remove('hidden')
-            collapseArrow.classList.add("open")
-        }
-    }
+    await waitForElements(() => document.getElementsByClassName('track-wrapper'), transformItems.length)
+}
 
-    // Collapsable titles
-    const playlistTitles = document.getElementsByClassName("playlist-title")
-    Array.from(playlistTitles).forEach((
-        /** @type {HTMLDivElement} */
-        playlistTitle) => {
-        const collapseArrow = document.createElement("span")
-        collapseArrow.classList = "fa fa-lg fa-caret-down collapse-caret"
-        playlistTitle.appendChild(collapseArrow)
-        setCollapsed(isMobile(), playlistTitle)
-
-        playlistTitle.addEventListener("click", function () {
-            const collapseContent = this.parentElement.getElementsByClassName('playlist-items')[0]
-            const isCollapsed = collapseContent.classList.contains('hidden')
-            setCollapsed(!isCollapsed, this)
-
-            if (isMobile() && isCollapsed) {
-                Array.from(playlistTitles).forEach((otherPlaylistTitleEl) => {
-                    if (otherPlaylistTitleEl !== this) {
-                        setCollapsed(true, otherPlaylistTitleEl)
-                    }
-
-                })
-            }
-
-        });
-    })
-
-
-    // Scrollable playlists
-    const scrollRegions = document.getElementsByClassName("scroll")
-
+function setPlaylistsScrollable() {
+    const scrollRegions = document.getElementsByClassName("playlist-items")
     Array.from(scrollRegions).forEach((scrollRegion) => {
+        if (!isScrollableY(scrollRegion)) return
+
         const ARROW_BTN_CLICK_SCROLL_DIST = 150
 
         const upDiv = document.createElement("div")
@@ -177,6 +197,27 @@ function init() {
             downArrow.style.display = isScrolledToBottom(thisScrollRegion, 50) ? "none" : "inherit"
         });
     })
+}
+
+
+async function init() {
+    // All anchors should open in NEW TAB
+    const allAnchors = document.getElementsByTagName("a")
+    Array.from(allAnchors).forEach((el) => {
+        el.target = "_blank"
+        el.rel = "noreferrer noopener"
+    })
+
+    setPlaylistTitlesCollapsable()
+
+    // Soundcloud stuff
+    SC.initialize({
+        client_id: 'DgFeY88vapbGCcK7RrT2E33nmNQVWX82'
+    });
+
+    await setScTracksElements(SC)
+
+    setPlaylistsScrollable()
 }
 
 window.onload = init
