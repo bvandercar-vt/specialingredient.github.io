3037037e857564deb96727739148734ef07b1c7c
bvandercar-vt <blake.vandercar@validatek.com>
2023-08-17 16:41:43 -0600

    attempt to fix

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,19 @@
+
+function fixHtml() {
+    return {
+        name: 'remove-module',
+        transformIndexHtml(html, id) {
+            return html.replaceAll('crossorigin', '').replaceAll('src="/', 'src="').replaceAll('href="/', 'href="')
+        },
+    };
+}
+
 /** @type {import('vite').UserConfig} */
 export default {
-    base: `/specialingredient.github.io/`
+    // base: `/specialingredient.github.io/`,
+    build: {
+        target: 'esnext',
+        modulePreload: false,
+    },
+    plugins: [fixHtml()]
 }
