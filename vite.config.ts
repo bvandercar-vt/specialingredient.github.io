d3245d32e3eab3d1348c25d77c62341d6bda5795
bvandercar-vt <blake.vandercar@validatek.com>
2023-08-17 18:37:21 -0600

    vite config to ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-/** @type {import('vite').UserConfig} */
-export default {
+import { defineConfig } from "vite"
+export default defineConfig(() => ({
     base: `/specialingredient.github.io/`,
     build: {
         target: 'esnext',
         modulePreload: false,
     },
-}
+}))
