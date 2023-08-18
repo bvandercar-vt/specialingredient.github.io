3cff04bb98bd21b5b8c378203b8fd161cf570a70
bvandercar-vt <blake.vandercar@validatek.com>
2023-08-17 19:15:53 -0600

    try new base

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 export default defineConfig(() => ({
-  base: `/specialingredientbass.com/`,
+  base: `/`,
   build: {
     target: 'esnext',
     modulePreload: false,
