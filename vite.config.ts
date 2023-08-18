87f2c71770fae88b24a19dc8af1a474695f77dca
bvandercar-vt <blake.vandercar@validatek.com>
2023-08-17 19:18:07 -0600

    change base

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 export default defineConfig(() => ({
-  base: `/`,
+  base: `specialingredientbass.com/`,
   build: {
     target: 'esnext',
     modulePreload: false,
