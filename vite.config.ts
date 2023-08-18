97d55dfb229dc2d28a771ca456780317ec0f1674
bvandercar-vt <blake.vandercar@validatek.com>
2023-08-17 19:12:53 -0600

    stylre: yarn format

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 export default defineConfig(() => ({
-    base: `/specialingredientbass.com/`,
-    build: {
-        target: 'esnext',
-        modulePreload: false,
-    },
+  base: `/specialingredientbass.com/`,
+  build: {
+    target: 'esnext',
+    modulePreload: false,
+  },
 }))
