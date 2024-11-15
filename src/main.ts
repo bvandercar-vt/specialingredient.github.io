0546f11f7bcb7290170e52e70eb8c6bb5a1529e5
bvandercar-vt <blake.vandercar@validatek.com>
2024-11-15 11:45:30 -0700

    only get soundcloud data in build

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,12 @@ import './styles/index.css'
 /**
  * Regular imports
  */
+import data from '../soundcloud-data.json'
+import { setSearchParams } from './api/api-utils'
 import { Classes, IFRAME_HEIGHT } from './constants'
 import {
   createElement,
+  htmlToElement,
   isMobile,
   isScrollableY,
   isScrolledToBottom,
@@ -142,6 +145,103 @@ function maybeSetScrollArrows(scrollRegion: HTMLElement) {
   onClassChange(scrollRegion, showOrHideArrows) // for when becomes hidden or unhidden
 }
 
+function setGridCardTitles() {
+  const gridCards = document.getElementsByClassName(Classes.GRID_CARD)
+  for (let i = 0; i < gridCards.length; i++) {
+    const gridCard = gridCards[i]
+    const cardUrl = gridCard.getAttribute('data-url')
+    const cardTitle = gridCard.getAttribute('data-title')
+    if (!cardUrl || !cardTitle) throw new Error('need data-url and data-title')
+
+    const labelId = `card-title-${i}`
+
+    const titleWrapper = createElement('div', {
+      classes: Classes.CARD_TITLE,
+      attributes: { href: cardUrl },
+      children: [
+        // title
+        createElement('h2', { attributes: { id: labelId }, children: cardTitle }),
+        // collapseArrow
+        createElement('span', {
+          classes: [`fa`, `fa-lg`, `fa-caret-down`, Classes.COLLAPSE_CARET],
+        }),
+      ],
+    })
+
+    gridCard.setAttribute('aria-labelledby', labelId)
+    gridCard.setAttribute('role', 'region')
+    gridCard.removeAttribute('data-url')
+    gridCard.removeAttribute('data-title')
+
+    gridCard.insertBefore(titleWrapper, gridCard.firstChild)
+  }
+}
+
+async function setSoundcloudTracks() {
+  const itemsToTransform = Array.from(document.getElementsByClassName(Classes.TRANSFORM_TO_SC_ITEM))
+  Promise.all(
+    itemsToTransform.map(async (itemToTransform) => {
+      const originalLink = itemToTransform.getAttribute('data-sc-link')
+      if (!originalLink) throw new Error('needs data-sc-link')
+
+      const oEmbed = data.find((d) => d.originalLink === originalLink)
+      if (!oEmbed) throw new Error(`no match for ${originalLink}`)
+
+      const titleStr =
+        itemToTransform.getAttribute('data-title') ??
+        oEmbed.title
+          .replaceAll(' by Special Ingredient', '')
+          .replaceAll('[w TRACKLIST]', '')
+          .replaceAll('[MASHUP]', '')
+
+      const genreDescription = itemToTransform.getAttribute('data-genre-desc')
+      const addlDescriptionSet = itemToTransform.getAttribute('data-addl-desc')
+      const addlDescription =
+        addlDescriptionSet === 'GET_FROM_SC' ? oEmbed.description : addlDescriptionSet
+
+      const trackWrapper = createElement('div', {
+        classes: Classes.TRACK_WRAPPER,
+        children: [
+          titleStr
+            ? createElement('p', { classes: Classes.TRACK_TITLE, children: titleStr })
+            : undefined,
+          genreDescription
+            ? createElement('p', { classes: Classes.TRACK_GENRE_DESC, children: genreDescription })
+            : undefined,
+          addlDescription
+            ? createElement('p', { classes: Classes.TRACK_ADDL_DESC, children: addlDescription })
+            : undefined,
+        ],
+      })
+
+      const iframeElement = htmlToElement(oEmbed.html) as HTMLIFrameElement
+      iframeElement.title = titleStr
+      const url = new URL(iframeElement.src)
+      setSearchParams(url, {
+        auto_play: false,
+        hide_related: false,
+        show_comments: true,
+        show_user: false,
+        show_reposts: true,
+        show_teaser: false,
+        visual: true, // true =  artwork behind waveform, false = artwork to left
+        show_artwork: true,
+      })
+      iframeElement.src = url.href
+      const scIframeWrapper = createElement('div', {
+        classes: Classes.SC_IFRAME_WRAPPER,
+        children: iframeElement,
+        // privacy policy cover
+        // createElement('div', { classes: Classes.PRIVACY_POLICY_COVER }),
+      })
+      trackWrapper.appendChild(scIframeWrapper)
+
+      itemToTransform.outerHTML = trackWrapper.outerHTML
+      itemToTransform.replaceWith(trackWrapper)
+    }),
+  )
+}
+
 function init() {
   // set global css var
   document.documentElement.style.setProperty('--iframe-height', `${IFRAME_HEIGHT}px`)
@@ -152,11 +252,20 @@ function init() {
   window.addEventListener('resize', setAppHeight)
   setAppHeight()
 
+  setGridCardTitles()
+
+  setSoundcloudTracks()
+
   setGridCardsCollapsible()
 
   Array.from(
     document.getElementsByClassName<HTMLDivElement>(Classes.CARD_COLLAPSE_CONTENT),
   ).forEach((collapseContent) => maybeSetScrollArrows(collapseContent))
+
+  // All anchors should open in NEW TAB
+  Array.from(document.getElementsByTagName('a')).forEach((el) => {
+    el.target = '_blank'
+  })
 }
 
 init()
