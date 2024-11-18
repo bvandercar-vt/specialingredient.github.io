6b72c1e8977a29a70c1a3ddae3263ea7e64ab62c
bvandercar-vt <blake.vandercar@validatek.com>
2024-11-18 01:57:41 -0700

    on play

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,41 @@
 import 'font-awesome/css/font-awesome.min.css'
 import './styles/index.css'
 
+import './api/soundcloud_player.js'
+
 import { GridBody } from './components/GridBody'
 import { Header } from './components/Header'
+
+type ScWidgetEventKeys =
+  | 'LOAD_PROGRESS'
+  | 'PLAY_PROGRESS'
+  | 'PLAY'
+  | 'PAUSE'
+  | 'FINISH'
+  | 'SEEK'
+  | 'READY'
+  | 'CLICK_DOWNLOAD'
+  | 'CLICK_BUY'
+  | 'OPEN_SHARE_PANEL'
+  | 'ERROR'
+
+declare global {
+  interface Window {
+    SC: {
+      Widget: {
+        (el: HTMLIFrameElement | string): {
+          play(): void
+          pause(): void
+          toggle(): void
+          bind(eventName: string, listener: () => void): void
+          unbind(eventName: string): void
+          load(url: string, options: Record<string, unknown>): void
+        }
+        Events: Record<ScWidgetEventKeys, string>
+      }
+    }
+  }
+}
 
 export const App = () => {
   return (
--- src/components/SoundcloudIframe.tsx
+++ src/components/SoundcloudIframe.tsx
@@ -1,4 +1,5 @@
 import classNames from 'classnames'
+import { useEffect, useId, useRef, useState } from 'react'
 import { setSearchParams } from '../utils/api-utils'
 import { htmlToElement } from '../utils/html-utils'
 
@@ -9,6 +10,10 @@ export interface SoundcloudIframeProps {
 }
 
 export const SoundcloudIframe = ({ html, title, className }: SoundcloudIframeProps) => {
+  const id = useId()
+  const wrapperRef = useRef<HTMLDivElement>(null)
+  const [isPlaying, setIsPlaying] = useState<boolean>(false)
+
   const dummyElement = htmlToElement(html) as HTMLIFrameElement
   dummyElement.title = title
   const iframeUrl = new URL(dummyElement.src)
@@ -19,15 +24,26 @@ export const SoundcloudIframe = ({ html, title, className }: SoundcloudIframePro
     show_user: false,
     show_reposts: true,
     show_teaser: false,
-    visual: true, // true =  artwork behind waveform, false = artwork to left
+    visual: true, // true =  artwork behind waveform, false = artwork to left. If false though, requires taller height to see comments
     show_artwork: true,
   })
   dummyElement.src = iframeUrl.href
+  dummyElement.id = id
+
+  useEffect(() => {
+    if (wrapperRef.current?.firstElementChild) {
+      const widget = window.SC.Widget(id)
+      widget.bind(window.SC.Widget.Events.PLAY, () => setIsPlaying(true))
+      widget.bind(window.SC.Widget.Events.PAUSE, () => setIsPlaying(false))
+      widget.bind(window.SC.Widget.Events.FINISH, () => setIsPlaying(false))
+    }
+  }, [wrapperRef.current?.firstElementChild])
 
   return (
     <div
-      className={classNames('sc-iframe-wrapper', className)}
+      className={classNames('sc-iframe-wrapper', className, { playing: isPlaying })}
       dangerouslySetInnerHTML={{ __html: dummyElement.outerHTML }}
+      ref={wrapperRef}
     />
   )
 }
