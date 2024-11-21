6ca354f1f54866a86a801faa11d336fa01aec508
bvandercar-vt <blake.vandercar@validatek.com>
2024-11-21 08:50:51 -0700

    prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,6 +3,14 @@ const config = {
   semi: false,
   singleQuote: true,
   printWidth: 100,
+  overrides: [
+    {
+      files: '*.json',
+      options: {
+        insertFinalNewLine: 'false',
+      },
+    },
+  ],
 }
 
 export default config
